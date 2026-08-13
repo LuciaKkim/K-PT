63431c24dd8d7b89e7a2f73c5cf95ffea962cd17
Minhee Han <yyj9694651@gmail.com>
2026-08-13 15:34:42 +0900

    [Chore] 기본 설정

---
+++ src/test/java/cloud/intensive/kpt/KptApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.intensive.kpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
